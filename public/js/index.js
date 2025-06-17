b7e73cbe248b4e675f49d7e2b7cf3138c1f5dfb3
Aaron L <daeige@hotmail.com>
2025-06-17 18:38:05 -0400

    Added function to retrieve random pictures from Unsplash

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,57 @@
+const YOUR_ACCESS_KEY = "";
 
+async function getRandomImage() {
+    const client_id = YOUR_ACCESS_KEY;
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-"use strict"
-
-const elements = {
-    quote: document.getElementById("quote"),
-    author: document.getElementById("author"),
-};
-
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
-
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "I think my assicle has fallen asleep.",
-        author: "Orm from Norsemen",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+// "use strict"
+
+// const elements = {
+//     quote: document.getElementById("quote"),
+//     author: document.getElementById("author"),
+// };
+
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",
+//     },
+
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+
+//     {
+//         quote: "I think my assicle has fallen asleep.",
+//         author: "Orm from Norsemen",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
